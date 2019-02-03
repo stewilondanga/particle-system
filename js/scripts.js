09bd71ec97e66607d692954d9d0b3cb984895370
Stewart ilondanga <stilondanga@gmail.com>
2019-02-03 09:46:50 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -81,11 +81,11 @@ Vector.prototype.getMagnitude = function() {
   return Math.sqrt(this.x * this.x + this.y * this.y);
 };
 
-/*Vector.prototype.getAngle = function () {
-  return Math.atan2(this.y,this.x);
+Vector.prototype.getAngle = function() {
+  return Math.atan2(this.y, this.x);
 };
 
-Vector.fromAngle = function (angle, magnitude) {
+/*Vector.fromAngle = function (angle, magnitude) {
   return new Vector(magnitude * Math.cos(angle), magnitude * Math.sin(angle));
 };
 
